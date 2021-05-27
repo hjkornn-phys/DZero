6ff9779c85940e74b5055d7e481347d395236101
HJkorNN <hjkornn@gmail.com>
2021-05-27 15:29:01 +0900

    edit step10

--- GOZI1/step10.py
+++ GOZI1/step10.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
             num_grad = numerical_diff(square, x)
             flg = np.allclose(x.grad, num_grad)
             self.assertTrue(flg)
+
     unittest.main()
 
 
